f10b7a1110043415b6ea98c6d8128f3705e0bbef
Ailenswpu <renkaiswpu@gmail.com>
2013-12-05 16:48:47 +0800

    update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,3 +6,4 @@ BooksShare::Application.config.session_store :cookie_store, key: '_books-share_s
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # BooksShare::Application.config.session_store :active_record_store
+ThinkingSphinx::Middlewares::DEFAULT.delete ThinkingSphinx::Middlewares::UTF8
